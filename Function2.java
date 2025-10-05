dc9e592ed64c9dc0ba90d3f3bf2674318715887e
Aditya-050306 <joshiaditya0503@gmail.com>
2025-10-05 22:22:31 +0530

    git commit -m "Product of 2 numbers using functions"

---
+++ Function2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Function2 {
+    public static int calculateProduct(int a,int b){
+        int product= a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter a and b ");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int product= calculateProduct(a,b);
+        System.out.println("Product of 2 Numbers= "+ product);
+        sc.close();
+    }
+}
