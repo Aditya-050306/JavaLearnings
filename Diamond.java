9fc1812f57c5ea3fd438584e36fc088a11160697
Aditya-050306 <joshiaditya0503@gmail.com>
2025-10-11 17:31:53 +0530

    Diamond Pattern in Java

--- Diamond.java
+++ Diamond.java
@@ -1,27 +1,30 @@
-
 import java.util.Scanner;
 
 public class Diamond {
     public static void main(String[] args) {
-        int n,i=1,j=1;
-        Scanner sc=new Scanner(System.in);
+        int n;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows: ");
-        n=sc.nextInt();
-        for(i=1;i<=n;i++){
-            for(j=1;j<=n;j++){
-                System.out.println(" ");
+        n = sc.nextInt();
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            for(j=1;j<=(2*i-1);j++){
-                System.out.println("*");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-        for(i=n-1;i>=1;i--){
-            for(j=n;j>i;j--){
-                System.out.println(" ");
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            for(j=1;j<=(2*i-1);j++){
-                System.out.println("*");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
